b9234efbe837c01d4aa7194ca872674005f6ceca
nyoko708 <ckgxd325@yahoo.co.jp>
2015-09-22 00:17:39 +0900

    is-unique-string.rb

---
+++ mondai/is-unique-string.rb
@@ -0,0 +1,25 @@
+#
+# 指定した文字列に重複があるかどうか判定する
+#
+
+str = 'あいうえおabcdefg'
+
+# 指定した文字列に重複があるかどうか判定する関数
+# @param [string] str 文字
+# @return [boolean] 重複がなければtrue
+def is_unique_string(str)
+
+  strs = str.split("")
+
+  is_unique_flags = {}
+  for str in strs do
+    if is_unique_flags.has_key?(str) && is_unique_flags[str] == true
+      return false
+    end
+    is_unique_flags[str] = true
+  end
+
+  return true
+end
+
+puts is_unique_string(str)
